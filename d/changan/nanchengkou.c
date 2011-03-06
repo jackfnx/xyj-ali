3e3a344fa986b53a90e0e5fc5fbf15bf5bdca4e4
NewX <newx@soumo.info>
2011-03-06 17:12:41 +0900

    greeting1/2

--- d/changan/nanchengkou.c
+++ d/changan/nanchengkou.c
@@ -10,8 +10,8 @@ inherit ROOM;
 
 void create ()
 {
-  set ("short", "南城口");
-  set ("long", @LONG
+    set ("short", "南城口");
+    set ("long", @LONG
 
 这里是长安的南城口。往北望去已可看到南城门络绎不绝的人流及城
 口守卫的官兵。南去一桥高跨泾水，而天际处终南山麓淡淡地横过你
@@ -19,23 +19,23 @@ void create ()
 气象。
 LONG);
 
-  set("exits", ([ /* sizeof() == 4 */
-  //"west" : __DIR__"wside1",
-  "south" : __DIR__"nbridge",
-  "north" : "/d/city/zhuque-s4",
-  //"east" : __DIR__"eside1",
-//  "southeast" : "/d/eastway/jinshi",
-]));
-        set("objects", 
-        ([ //sizeof() == 1
-                "/d/city/npc/wujiang" : 1,
-                "/d/city/npc/bing" : 3,
-        ]));
+    set("exits", ([ /* sizeof() == 4 */
+            //"west" : __DIR__"wside1",
+            "south" : __DIR__"nbridge",
+            "north" : "/d/city/zhuque-s4",
+            //"east" : __DIR__"eside1",
+            //  "southeast" : "/d/eastway/jinshi",
+    ]));
+    
+    set("objects", 
+    ([ //sizeof() == 2
+            "/d/city/npc/wujiang" : 1,
+            "/d/city/npc/bing" : 3,
+    ]));
 
+    set("outdoors", 1);
 
-  set("outdoors", 1);
-
-  setup();
+    setup();
 }
 
 
--- d/kaifeng/chengmen.c
+++ d/kaifeng/chengmen.c
@@ -7,8 +7,8 @@ inherit ROOM;
 
 void create ()
 {
-  set ("short", "开封城门");
-  set ("long", @LONG
+    set ("short", "开封城门");
+    set ("long", @LONG
 
 一座高高的青垒石墙城楼耸立，城门上旌旗招展。城下是一条宽宽的
 大道从门洞里穿过，往东就是城里，大道西边通向远方。门两边站立
@@ -16,19 +16,18 @@ void create ()
 
 LONG);
 
-  set("exits", ([
-        "west" : "/d/eastway/east5",
-        "east" : __DIR__"chen1",
-      ]));
-
-  set("objects", 
-      ([
-         __DIR__"npc/wujiang" : 1,
-         __DIR__"npc/bing" : 3,
-      ]));
-  set("outdoors", __DIR__);
-
-  setup();
+    set("exits", ([
+            "west" : "/d/eastway/east5",
+            "east" : __DIR__"chen1",
+    ]));
+    
+    set("objects", ([
+            "/d/city/npc/wujiang" : 1,
+            "/d/city/npc/bing" : 3,
+    ]));
+    set("outdoors", __DIR__);
+    
+    setup();
 }
 
 
