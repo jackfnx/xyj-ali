b9036f952d408ed73eff156b11c99f5421d257c2
NewX <newx@soumo.info>
2011-03-30 01:27:45 +0800

    jindouyun->cloud-steps

--- daemon/skill/lingtai/cloud-steps.c
+++ daemon/skill/lingtai/cloud-steps.c
@@ -1,7 +1,7 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
  
-// jindouyun.c
+// cloud-steps.c
 
 inherit SKILL;
 
@@ -12,7 +12,6 @@ string *dodge_msg = ({
    "$n左足一点，一招「穿云式」腾空而起，避了开去。\n",
    "但是$n使一招「流云步」，身子轻轻飘了开去。\n",
    "可是$n胸腹往后一缩，在间不容发之际一个筋斗倒翻而出，好个「倒骑云」！\n",
-   "只见$n一声长啸，卷起一股疾风，身形亦腾风而起，\n$N的攻击早失了准头。正是“筋斗云”中的「腾云式」！\n"
 });
 
 int valid_enable(string usage)
@@ -23,7 +22,7 @@ int valid_enable(string usage)
 int valid_learn(object me)
 {
    if( (int)me->query("max_force") < 0 )
-     return notify_fail("你的内力不够，没有办法练筋斗云。\n");
+     return notify_fail("你的内力不够，没有办法练腾云术。\n");
 
    return 1;
 }
@@ -37,7 +36,7 @@ int practice_skill(object me)
 {
    if( (int)me->query("kee") < 30
    ||   (int)me->query("force") < 3 )
-     return notify_fail("你的气或内力不够，不能练筋斗云。\n");
+     return notify_fail("你的气或内力不够，不能练腾云术。\n");
    me->receive_damage("kee", 30);
    me->add("force", -3);
    return 1;
