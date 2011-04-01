fca4b63d6c275a5e6255c641f73803a2d1d29ea9
NewX <newx@soumo.info>
2011-04-01 23:04:20 +0800

    CMD:chinese

--- cmds/arch/chinese.c
+++ cmds/arch/chinese.c
@@ -5,25 +5,32 @@ inherit F_CLEAN_UP;
 
 int main(object me, string arg)
 {
-   string key, chinz;
-
-   if( !arg || arg=="" )
-     return notify_fail("指令格式：chinese <英文>==<中文>\n");
-
-   if( sscanf(arg, "%s==%s", key, chinz)==2 ) {
-     CHINESE_D->add_translate(key, chinz);
-     write( key + " == " + chinz + "\nOk.\n");
-     return 1;
-   }
-
-   return 0;
+    string key, chinz;
+    
+    if( !arg || arg=="" )
+        return notify_fail("指令格式：chinese <英文>==<中文>\n");
+    
+    if( sscanf(arg, "%s==%s", key, chinz)==2 ) {
+        if (chinz != "") {
+            CHINESE_D->add_translate(key, chinz);
+            write( key + " == " + chinz + "\nOk.\n");
+        }
+        else {
+            CHINESE_D->remove_translate(key);
+            write(key + " remove OK.\n");
+        }
+        return 1;
+    }
+    
+    return 0;
 }
 
 int help()
 {
-   write ( @HELP
-指令格式：chinese <英文>==<中文>
+    write ( @HELP
+增加对照：chinese <英文>==<中文>
+删除对照：chinese <英文>==<>
 HELP
-   );
-   return 1 ;
+    );
+    return 1 ;
 }
