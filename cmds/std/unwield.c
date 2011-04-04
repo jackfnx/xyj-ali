0383471806982d785e4dc8ca5d01fb9068fda4b0
NewX <newx@soumo.info>
2011-04-05 02:40:29 +0800

    unwield all

--- cmds/std/unwield.c
+++ cmds/std/unwield.c
@@ -7,25 +7,47 @@ inherit F_CLEAN_UP;
 
 int main(object me, string arg)
 {
-   object ob, *inv;
-   string str;
-   int i;
+    object ob, *inv;
+    string str;
+    int i;
 
-   if( !arg ) return notify_fail("你要脱掉什么？\n");
+    if (!arg) return notify_fail("你要脱掉什么？\n");
 
-   if( !objectp(ob = present(arg, me)) )
-     return notify_fail("你身上没有这样东西。\n");
+    if (arg == "all") {
+        if (objectp(ob = me->query_temp("secondary_weapon"))) {
+            if (ob->unequip()) {
+                if (!stringp(str = ob->query("unwield_msg")))
+                    str = "$N放下手中的$n。\n";
+                message_vision(str, me, ob);
+            }
+        }
+        if (objectp(ob = me->query_temp("weapon"))) {
+            if (ob->unequip()) {
+                if (!stringp(str = ob->query("unwield_msg")))
+                    str = "$N放下手中的$n。\n";
+                message_vision(str, me, ob);
+                return 1;
+            }
+            else
+                return 0;
+        }
+        else
+            return notify_fail("你没有装备武器。\n");
+    }
 
-   if( (string)ob->query("equipped")!="wielded"&&(string)ob->query("equipped")!="second_wield" )
-     return notify_fail("你并没有装备这样东西作为武器。\n");
+    if (!objectp(ob = present(arg, me)))
+        return notify_fail("你身上没有这样东西。\n");
 
-   if( ob->unequip() ) {
-     if( !stringp(str = ob->query("unwield_msg")) )
-        str = "$N放下手中的$n。\n";
-     message_vision(str, me, ob);
-     return 1;
-   } else
-     return 0;
+    if ((string)ob->query("equipped")!="wielded"&&(string)ob->query("equipped")!="second_wield")
+        return notify_fail("你并没有装备这样东西作为武器。\n");
+
+    if (ob->unequip()) {
+        if (!stringp(str = ob->query("unwield_msg")))
+            str = "$N放下手中的$n。\n";
+        message_vision(str, me, ob);
+        return 1;
+    } else
+        return 0;
 }
 
 int help(object me)
