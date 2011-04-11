06619dc5ddc084922a673a55fbc2621fe65e063a
NewX <NewX@soumo.info>
2011-04-11 15:35:09 +0800

    obj.h

--- include/obj.h
+++ include/obj.h
@@ -7,10 +7,5 @@
 #define __OBJ__H
 
 #define NPC_DEMOGORGON        "/obj/npc/demogorgon"
-#define NPC_GARRISON        "/obj/npc/garrison"
-#define NPC_HEAVEN_SOLDIER     "/obj/npc/heaven_soldier"
-#define NPC_ZOMBIE          "/obj/npc/zombie"
-#define NPC_MIND_BUG        "/obj/npc/mind_bug"
-#define NPC_MIND_BEAST        "/obj/npc/mind_beast"
 
 #endif
