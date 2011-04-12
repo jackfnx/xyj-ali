c7ec7aeb6f302d92fa0b0e609f84c54863f7dd70
NewX <NewX@soumo.info>
2011-04-12 17:06:11 +0800

    necromancy

--- daemon/skill/hell/necromancy.c
+++ daemon/skill/hell/necromancy.c
@@ -1,7 +1,7 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
  
-// 勾魂术
+// 死灵术
 
 inherit SKILL;
 
@@ -9,16 +9,14 @@ int valid_enable(string usage) { return usage=="spells"; }
 
 int valid_learn(object me)
 {
-        if( (int)me->query_skill("spells", 1) < 10
-        ||      (int)me->query_skill("spells", 1) <=
-(int)me->query_skill("gouhunshu", 1) )
-                return
-notify_fail("你的法术修为还不够高深，无法学习勾魂术．\n");
-        return 1;
+    if ((int)me->query_skill("spells", 1) < 10
+    ||  (int)me->query_skill("spells", 1) <= (int)me->query_skill("necromancy", 1) )
+        return notify_fail("你的法术修为还不够高深，无法学习死灵术．\n");
+    return 1;
 }
 
 string cast_spell_file(string spell)
 {
-        return CLASS_D("ghost") + "/gouhunshu/" + spell;
+    return CLASS_D("ghost") + "/necromancy/" + spell;
 }
 
