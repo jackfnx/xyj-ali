fe4576c1c6f7f039928932301ef1a4bd8fdfa4a3
NewX <NewX@soumo.info>
2011-04-12 17:17:16 +0800

    tonsillitis->tonsillit

--- daemon/skill/hell/tonsillit.c
+++ daemon/skill/hell/tonsillit.c
@@ -1,7 +1,7 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
  
-// celestial.c
+// tonsillit.c
 
 inherit FORCE;
 
@@ -14,18 +14,18 @@ int valid_learn(object me)
         if( (int)me->query("bellicosity") < (int)me->query_skill("tonsillit") * 40 )
                 return notify_fail("你的杀气不够，无法领悟更高深的摄气诀。\n");
 */
-   if( (int)me->query_skill("tonsillit", 1) >= 250 )
-     return notify_fail("你的气量有限，对摄气诀已领悟到了极限。\n");
-        return 1;
+    if ((int)me->query_skill("tonsillit", 1) >= 250)
+        return notify_fail("你的气量有限，对摄气诀已领悟到了极限。\n");
+    return 1;
 }
 
 int practice_skill(object me)
 {
-        return notify_fail("摄气诀只能用学习或应用中提高！\n");
+    return notify_fail("摄气诀只能用学习或应用中提高！\n");
 }
 
 string exert_function_file(string func)
 {
-        return CLASS_D("ghost") + "/tonsillitis/" + func;
+    return CLASS_D("ghost") + "/tonsilliti/" + func;
 }
 
