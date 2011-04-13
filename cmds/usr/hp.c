75a0209aa62c1382c033770fedf5253a49bf9c23
NewX <NewX@soumo.info>
2011-04-13 14:39:55 +0800

    hp spelling

--- cmds/usr/hp.c
+++ cmds/usr/hp.c
@@ -38,7 +38,7 @@ int main(object me, string arg)
     
     my = ob->query_entire_dbase();
 
-    printf(" 真元： %s%4d/ %4d %s(%3d%%)" NOR "    灵力： %s%4d / %4d (+%d)\n" NOR,
+    printf(" 精魂： %s%4d/ %4d %s(%3d%%)" NOR "    真元： %s%4d / %4d (+%d)\n" NOR,
         status_color(my["gin"], my["eff_gin"]),   my["gin"],   my["eff_gin"],
         status_color(my["eff_gin"], my["max_gin"]),   my["eff_gin"] * 100 / my["max_gin"],
         status_color(my["atman"], my["max_atman"]),   my["atman"], my["max_atman"],
