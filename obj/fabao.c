5d9ef23e77d80c70641949bb0783e4ea1b11119a
NewX <NewX@soumo.info>
2011-04-13 09:47:59 +0800

    fabao_ob of paiban

--- obj/fabao.c
+++ obj/fabao.c
@@ -13,14 +13,14 @@ inherit F_BACKUP;
 
 void create()
 {
-   seteuid(0);   
-   set_name("自制法宝物件", ({ "fabao object", "fabao", "object" }) );
-   setup();
+    seteuid(0);   
+    set_name("自制法宝物件", ({ "fabao object", "fabao", "object" }));
+    setup();
 }
 
 void reset()
 {
-    if( !environment() ||  //mon 2/4/98
+    if (!environment() ||  //mon 2/4/98
         query("owner_id") != getuid(environment()) )  {
         destruct(this_object());
         return;
@@ -30,27 +30,28 @@ void reset()
 // This is used by F_SAVE to determine the filename to save our data.
 string query_save_file()
 {
-   string id, series;
+    string id
+    int series;
 
-   id = query("owner_id");
-   series = query("series_no");
-   if( !id || !series) return 0;
+    id = query("owner_id");
+    series = query("series_no");
+    if (!id || !series) return 0;
 
-   return sprintf(DATA_DIR "fabao/%c/%s-%d", id[0], id, series);
+    return sprintf(DATA_DIR "fabao/%c/%s-%d", id[0], id, series);
 }
 
 int save()
 {
-   if( query("equipped") )   {
-     write("必须放下法宝才能储存。\n");
-     return 0;
-   }
-   return ::save();
+    if (query("equipped"))   {
+        write("必须放下法宝才能储存。\n");
+        return 0;
+    }
+    return ::save();
 }
 
 void setup()
 {
-   ::setup();
+    ::setup();
 }
 
 int query_unique()  { return 1; }
@@ -60,7 +61,7 @@ string extra_long()
     string str = "\n自制法宝等级：\n";
     int series_no, damage_stars, force_stars, i;
     int dodge_stars, armor_stars, vs_force_stars, spells_stars, vs_spells_stars;
-    
+
     if (query("fabao_type") == "weapon")  {
         damage_stars = query("stars/damage");
         force_stars = query("stars/force");
@@ -103,6 +104,6 @@ string extra_long()
             str += "★";
         str += NOR + "\n";
     }
-    
+
     return str;
 }
