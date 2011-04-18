5864a0525a70cfa5f6bb8681c852694e18654195
NewX <newx@soumo.info>
2011-04-18 23:29:05 +0800

    new more from xkx

--- feature/more.c
+++ feature/more.c
@@ -7,35 +7,38 @@
 
 varargs void more(string cmd, string *text, int line, int force)
 {
-  int i,j;
+    int i,j;
 
-  switch(cmd) {
-  case "":
-  case "f":
-    for(i=line + 23; line<sizeof(text) && line<i; line++)
-     write(text[line] + "\n");
-    if( line>=sizeof(text) && !force) return;
-    break;
-  case "b":
-  line = line - 46;
-  if(line<-22 && !force) return;
-  for(i=line + 23; line < i;line++)
-   write(text[line]+"\n");
-  break;
-   case "q":
-    return;
-  }
-  printf("== 未完继续 " HIY "%d%%" NOR 
-   " == (ENTER 继续下一页，q 离开，b 前一页)",
-   (line*100/sizeof(text)) );
-  input_to("more", text, line);
+    switch(cmd) {
+        case "b":
+            line = line - 46;
+            if (line <- 22 && !force) return;
+                write("\n"+ESC+"[1A"+ESC+"[100D"+ESC+"[K\n"+repeat_string("-",100)+"\n");
+            for (i = line + 23; line < i; line++)
+                write(text[line] + "\n");
+            break;
+        case "q":
+            write(ESC+"[1A"+ESC+"[100D"+ESC+"[K\n");
+            return;
+        case "":
+        case "f":
+        default:
+            write(ESC+"[1A"+ESC+"[100D"+ESC+"[K");
+            for(i = line + 23; line < sizeof(text) && line < i; line++)
+                write(text[line] + "\n");
+            if (line >= sizeof(text) && !force) return;
+            break;
+    }
+    printf("== 未完继续 " HIY "%d%%" NOR " == ("HIG"q"NOR" 离开，"HIG"b"NOR" 前一页，"HIG"[其他键]"NOR" 继续下一页)",
+        (line*100/sizeof(text)));
+    input_to("more", text, line);
 }
 
 varargs void start_more(string msg, int force)
 {
-    if(!msg || strlen(msg)<1) // no messages.
-   return;
-  //11/13/97 try to prevent crash by mon.
+    if (!msg || strlen(msg) < 1) // no messages.
+        return;
+    //11/13/97 try to prevent crash by mon.
 
-  more("", explode(msg, "\n"), 0, force);
+    more("", explode(msg, "\n"), 0, force);
 }
