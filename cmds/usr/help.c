ff4c99c0c9b08eb467641a2542d39ab262f991f4
NewX <NewX@soumo.info>
2011-04-20 16:00:56 +0800

    help

--- cmds/usr/help.c
+++ cmds/usr/help.c
@@ -21,30 +21,28 @@ int main(object me, string arg)
     string skill, verb;
     
     // If no topic specified, give the topics.
-    if( !arg ) {
+    if (!arg) {
         cat(HELP_DIR + "help/topics");
         return 1;
     }
 
     // Else, try if a command name is specified.
     seteuid(getuid());
-    if( stringp(file = me->find_command(arg)) ) {
+    if (stringp(file = me->find_command(arg))) {
         notify_fail("有这个指令存在，但是并没有详细的说明文件。\n");
         return file->help(me);
     }
     //Now see if it aliases to a command.
     //mon 10/27/97
-    if( stringp(file =
-        me->find_command(ALIAS_D->process_global_alias(arg))) ) {
+    if (stringp(file = me->find_command(ALIAS_D->process_global_alias(arg)))) {
         notify_fail("有这个指令存在，但是并没有详细的说明文件。\n");
         return file->help(me);
     }
-    
     // Search the individual search path first.
-    if( pointerp(search = me->query("help_search_path")) ) {
+    if (pointerp(search = me->query("help_search_path"))) {
         i = sizeof(search);
-        while(i--) if( file_size(search[i] + arg)>0 ) {
-            me->start_more( read_file(search[i] + arg) );
+        while(i--) if(file_size(search[i] + arg) > 0) {
+            me->start_more(read_file(search[i] + arg));
             return 1;
         }
     }
@@ -54,12 +52,12 @@ int main(object me, string arg)
     // Using a "()" after the function name can prevent it being
     // interpreted as a user command with the same name. -- mon
     sscanf(arg, "%s()", arg);
-    arg=replace_string(arg, "/", "");
+    arg = replace_string(arg, "/", "");
     
-    if (strsrch(arg,"*")>=0 || strsrch(arg,"?")>=0) wildcard=1;
-    else wildcard=0;
+    if (strsrch(arg, "*") >= 0 || strsrch(arg, "?") >= 0) wildcard = 1;
+    else wildcard = 0;
     
-    found=0;
+    found = 0;
     write("\n");
     
     // Finally, search the default search paths.
@@ -68,10 +66,10 @@ int main(object me, string arg)
         default_search = DEFAULT_SEARCH_PATHS;
     else
         default_search = DEFAULT_PLAYER_SEARCH_PATHS;
-    if ( pointerp(default_search) ) {
-        if (search_dir(default_search, arg, me, wildcard, wizardp(me))>0) {
-            if(wildcard) write("找到"+chinese_number(found)+
-                "个相关主题。\n");
+    if (pointerp(default_search)) {
+        if (search_dir(default_search, arg, me, wildcard, wizardp(me)) > 0) {
+            if (wildcard)
+                write("找到"+chinese_number(found)+ "个相关主题。\n");
             return 1;
         }
     }
@@ -87,45 +85,43 @@ int search_dir(string *dirs, string arg, object me, int wildcard, int wizp)
     mixed *filenames, *sub_dir;
 
     while(i--) {
-      filenames=get_dir(dirs[i] + arg, -1);
-
-      if(pointerp(filenames) && sizeof(filenames)) {
-        if(!wildcard && filenames[0][1]>0) {
-   //without wildcard, filenames[0] must be the only match.
-   //which should be the same as "dirs[i] + arg"
-     me->start_more( read_file(dirs[i] + arg) );
-     return 1;
-        }
-
-   if(wildcard) {
-          j=sizeof(filenames);
-     while(j--) {
-            if(filenames[j][1]>0) {
-         write("help "+filenames[j][0]+
-           (wizp?"     ("+dirs[i]+")":"")+ "\n");
-         found++;
+        filenames = get_dir(dirs[i] + arg, -1);
+        if (pointerp(filenames) && sizeof(filenames)) {
+            //without wildcard, filenames[0] must be the only match.
+            //which should be the same as "dirs[i] + arg"
+            if (!wildcard && filenames[0][1] > 0) {
+                me->start_more(read_file(dirs[i] + arg));
+                return 1;
             }
-     }
-   }
-      }
 
-      //check sub_directories
-      if(pointerp(filenames=get_dir(dirs[i], -1))) {
-        if(pointerp(sub_dir=filter_array(filenames, "find_sub"))) {
-     string *dirs1=({});
-
-          j=sizeof(sub_dir);
-     while(j--) {
-       dirs1+=({dirs[i]+sub_dir[j][0]+"/"}); 
-     }
-
-     if(search_dir(dirs1, arg, me, wildcard, wizp)==1 
-       && !wildcard) return 1;
+            if (wildcard) {
+                j = sizeof(filenames);
+                while (j--) {
+                    if (filenames[j][1] > 0) {
+                        write("help "+filenames[j][0]+(wizp?"     ("+dirs[i]+")":"")+"\n");
+                        found++;
+                    }
+                }
+            }
         }
-      }
+
+        //check sub_directories
+        if (pointerp(filenames = get_dir(dirs[i], -1))) {
+            if(pointerp(sub_dir = filter_array(filenames, "find_sub"))) {
+                string *dirs1 = ({});
+
+                j = sizeof(sub_dir);
+                while (j--) {
+                    dirs1 += ({dirs[i]+sub_dir[j][0]+"/"}); 
+                }
+
+                if (search_dir(dirs1, arg, me, wildcard, wizp) == 1 && !wildcard)
+                    return 1;
+            }
+        }
     }
 
-    return (wildcard*found);
+    return (wildcard * found);
 }
 
 int find_sub(mixed *filename)
