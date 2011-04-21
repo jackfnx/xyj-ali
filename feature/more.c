6f79fd730275760e6061d3e1a42fc6791e1df7c5
NewX <newx@soumo.info>
2011-04-21 19:15:08 +0800

    new more

--- feature/more.c
+++ feature/more.c
@@ -7,39 +7,44 @@
 
 varargs void more(string cmd, string *text, int line, int force)
 {
-    int i,j;
+    int i, len = strlen("" + sizeof(text));
 
     switch(cmd) {
-        case "b":
-            line = line - 46;
-            if (line < -22 && !force) return;
-            write("\n"+ESC+"[1A"+ESC+"[100D"+ESC+"[K\n"+repeat_string("-",100)+"\n");
-            for (i = line + 23; line < i; line++)
-                write(text[line] + "\n");
-            break;
         case "q":
             write(ESC+"[1A"+ESC+"[100D"+ESC+"[K\n");
             return;
-        case "":
+        case "b":
+            line = line - 46;
+            if (line < -22 && !force) return;
+            write("\n"+ESC+"[1A"+ESC+"[100D"+ESC+"[K"+repeat_string("-",100)+"\n");
+            for (i = line + 23; line < i; line++)
+                if (line >= 0) printf(CYN"%."+len+"d"NOR" %s\n", line, text[line]);
+            break;
         case "f":
-        default:
             write(ESC+"[1A"+ESC+"[100D"+ESC+"[K");
             for(i = line + 23; line < sizeof(text) && line < i; line++)
-                write(text[line] + "\n");
+                printf(CYN"%."+len+"d"NOR" %s\n", line, text[line]);
+            if (line >= sizeof(text) && !force) return;
+            break;
+        case "":
+        default:
+            write(ESC+"[1A"+ESC+"[100D"+ESC+"[K");
+            for(i = line + 1; line < sizeof(text) && line < i; line++)
+                printf(CYN"%."+len+"d"NOR" %s\n", line, text[line]);
             if (line >= sizeof(text) && !force) return;
             break;
     }
-    printf("== 未完继续 " HIY "%d%%" NOR " == ("HIG"q"NOR" 离开，"HIG"b"NOR" 前一页，"HIG"[其他键]"NOR" 继续下一页)",
+    printf("== 未完继续 "HIY"%d%%"NOR" == ("HIG"q"NOR" 离开，"HIG"b"NOR" 前一页，"HIG"f"NOR" 后一页，"HIG"[其他键]"NOR" 滚动)",
         (line*100/sizeof(text)));
     input_to("more", text, line);
 }
 
 varargs void start_more(string msg, int force)
 {
+    //11/13/97 try to prevent crash by mon.
     if (!msg || strlen(msg) < 1) // no messages.
         return;
-    //11/13/97 try to prevent crash by mon.
 
     write("\n");
-    more("", explode(msg, "\n"), 0, force);
+    more("f", explode(msg, "\n"), 0, force);
 }
