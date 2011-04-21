821b26e99dcf66e3101e65887afa57de8fa5a94a
NewX <NewX@soumo.info>
2011-04-21 10:31:02 +0800

    chinese

--- adm/simul_efun/chinese.c
+++ adm/simul_efun/chinese.c
@@ -1,19 +1,13 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
 
 string chinese_number(int i)
 {
-   return CHINESE_D->chinese_number(i);
-}
-
-string to_chinese(string str)
-{
-   return CHINESE_D->chinese(str);
+    return CHINESE_D->chinese_number(i);
 }
 
 int is_chinese(string str)
 {
-   if( strlen(str)>=2 && str[0] > 160 && str[0] < 255 ) return 1;
-   return 0;
+    if (strlen(str)>=2 && str[0] > 160 && str[0] < 255) return 1;
+    else return 0;
 }
