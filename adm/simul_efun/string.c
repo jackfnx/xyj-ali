b03af0809b728f8eb419e3b3f3cc5e4eb04e9557
NewX <NewX@soumo.info>
2011-05-05 17:01:21 +0800

    color_len

--- adm/simul_efun/string.c
+++ adm/simul_efun/string.c
@@ -1,20 +1,97 @@
-string ansi_filter(string str)
+#include <ansi.h>
+
+// calculate the color char in a string
+int color_len(string str)
 {
-    str = replace_string(str, "$BLK$", "");
-    str = replace_string(str, "$RED$", "");
-    str = replace_string(str, "$GRN$", "");
-    str = replace_string(str, "$YEL$", "");
-    str = replace_string(str, "$BLU$", "");
-    str = replace_string(str, "$MAG$", "");
-    str = replace_string(str, "$CYN$", "");
-    str = replace_string(str, "$WHT$", "");
-    str = replace_string(str, "$HIR$", "");
-    str = replace_string(str, "$HIG$", "");
-    str = replace_string(str, "$HIY$", "");
-    str = replace_string(str, "$HIB$", "");
-    str = replace_string(str, "$HIM$", "");
-    str = replace_string(str, "$HIC$", "");
-    str = replace_string(str, "$HIW$", "");
-    str = replace_string(str, "$NOR$", "");
-    return str;
+    int i;
+    int extra;
+
+    extra = 0;
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] == ESC[0]) {
+            while ((extra++, str[i] < 'A' ||
+                     str[i] > 'z' ||
+                     str[i] > 'Z' && str[i] < 'a') && i < strlen(str))
+                i++;
+        }
+    }
+    return extra;
+}
+
+// filter color
+string filter_color(string arg)
+{
+    arg = replace_string(arg, BLK, "");
+    arg = replace_string(arg, RED, "");
+    arg = replace_string(arg, GRN, "");
+    arg = replace_string(arg, YEL, "");
+    arg = replace_string(arg, BLU, "");
+    arg = replace_string(arg, MAG, "");
+    arg = replace_string(arg, CYN, "");
+    arg = replace_string(arg, WHT, "");
+    arg = replace_string(arg, HIR, "");
+    arg = replace_string(arg, HIG, "");
+    arg = replace_string(arg, HIY, "");
+    arg = replace_string(arg, HIB, "");
+    arg = replace_string(arg, HIM, "");
+    arg = replace_string(arg, HIC, "");
+    arg = replace_string(arg, HIW, "");
+    arg = replace_string(arg, NOR, "");
+    arg = replace_string(arg, BOLD, "");
+    arg = replace_string(arg, BLINK, "");
+    return arg;
+}
+
+// trans_color
+// raw = 0, only forecolor
+// raw = 1, include backround color
+// raw = 2, include blink & others
+varargs string trans_color(string arg, int raw)
+{
+    // forecolor
+    arg = replace_string(arg, "$BLK$", BLK);
+    arg = replace_string(arg, "$RED$", RED);
+    arg = replace_string(arg, "$GRN$", GRN);
+    arg = replace_string(arg, "$YEL$", YEL);
+    arg = replace_string(arg, "$BLU$", BLU);
+    arg = replace_string(arg, "$MAG$", MAG);
+    arg = replace_string(arg, "$CYN$", CYN);
+    arg = replace_string(arg, "$WHT$", WHT);
+    arg = replace_string(arg, "$HIR$", HIR);
+    arg = replace_string(arg, "$HIG$", HIG);
+    arg = replace_string(arg, "$HIY$", HIY);
+    arg = replace_string(arg, "$HIB$", HIB);
+    arg = replace_string(arg, "$HIM$", HIM);
+    arg = replace_string(arg, "$HIC$", HIC);
+    arg = replace_string(arg, "$HIW$", HIW);
+    arg = replace_string(arg, "$NOR$", NOR);
+
+    if (raw) {
+        // Background color
+        arg = replace_string(arg, "$BBLK$", BBLK);
+        arg = replace_string(arg, "$BRED$", BRED);
+        arg = replace_string(arg, "$BGRN$", BGRN);
+        arg = replace_string(arg, "$BYEL$", BYEL);
+        arg = replace_string(arg, "$BBLU$", BBLU);
+        arg = replace_string(arg, "$BMAG$", BMAG);
+        arg = replace_string(arg, "$BCYN$", BCYN);
+        arg = replace_string(arg, "$HBRED$", HBRED);
+        arg = replace_string(arg, "$HBGRN$", HBGRN);
+        arg = replace_string(arg, "$HBYEL$", HBYEL);
+        arg = replace_string(arg, "$HBBLU$", HBBLU);
+        arg = replace_string(arg, "$HBMAG$", HBMAG);
+        arg = replace_string(arg, "$HBCYN$", HBCYN);
+
+        if (raw == 2) {
+            // special effect
+            arg = replace_string(arg, "$U$", U);
+            arg = replace_string(arg, "$BLINK$", BLINK);
+            arg = replace_string(arg, "$REV$", REV);
+            arg = replace_string(arg, "$HIREV$", HIREV);
+            arg = replace_string(arg, "$BOLD$", BOLD);
+        }
+    }
+
+    arg += NOR;
+    return arg;
 }
