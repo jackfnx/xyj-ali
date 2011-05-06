9d6b1225fd4e57281bb8dc0871935c3e909570be
NewX <NewX@soumo.info>
2011-05-06 17:29:33 +0800

    snowmaze_inheritable

---
+++ d/xueshan/snowmaze.c
@@ -0,0 +1,72 @@
+inherit ROOM;
+
+void init();
+void reset_path(object);
+int check_out(object);
+
+void create()
+{
+    set("short", "雪海孤旅");
+    set("long", @LONG
+
+北风呼啸，飞雪茫茫，四面望去均是绵延不尽的雪峰。天地之间
+似已毫无生迹，除了雪还是雪。不过让你惊奇不已的是前面居然
+还有两串脚印，虽然都快淡得看不出来了，毕竟是人的脚印。你
+不由得一阵狂喜，不顾一切地追了上去。
+LONG);
+
+    set("outdoors", "xueshan");
+
+    setup();
+}
+
+void init()
+{
+    object me = this_player();
+    string xmaze = this_object()->query("xmaze");
+    string exit = this_object()->query("exit");
+
+    if (me->query_temp(xmaze) == 0)
+        me->set_temp(xmaze, 1);
+    else if (me->query_temp(xmaze) == 1)
+        reset_path(me);
+
+    if (check_out(me)) me->move(exit);
+}
+
+void reset_path(object me)
+{
+    if (me->query_temp("xmaze1")) me->delete_temp("xmaze1");
+    if (me->query_temp("xmaze2")) me->delete_temp("xmaze2");
+    if (me->query_temp("xmaze3")) me->delete_temp("xmaze3");
+    if (me->query_temp("xmaze4")) me->delete_temp("xmaze4");
+    if (me->query_temp("xmaze5")) me->delete_temp("xmaze5");
+    if (me->query_temp("xmaze6")) me->delete_temp("xmaze6");
+    if (me->query_temp("xmaze7")) me->delete_temp("xmaze7");
+    if (me->query_temp("xmaze8")) me->delete_temp("xmaze8");
+    if (me->query_temp("xmaze9")) me->delete_temp("xmaze9");
+    return;
+}
+
+int check_out(object me)
+{
+    if (me->query_temp("xmaze1") != 1) return 0;
+    if (me->query_temp("xmaze2") != 1) return 0;
+    if (me->query_temp("xmaze3") != 1) return 0;
+    if (me->query_temp("xmaze4") != 1) return 0;
+    if (me->query_temp("xmaze5") != 1) return 0;
+    if (me->query_temp("xmaze6") != 1) return 0;
+    if (me->query_temp("xmaze7") != 1) return 0;
+    if (me->query_temp("xmaze8") != 1) return 0;
+    if (me->query_temp("xmaze9") != 1) return 0;
+    if (me->query_temp("xmaze1")) me->delete_temp("xmaze1");
+    if (me->query_temp("xmaze2")) me->delete_temp("xmaze2");
+    if (me->query_temp("xmaze3")) me->delete_temp("xmaze3");
+    if (me->query_temp("xmaze4")) me->delete_temp("xmaze4");
+    if (me->query_temp("xmaze5")) me->delete_temp("xmaze5");
+    if (me->query_temp("xmaze6")) me->delete_temp("xmaze6");
+    if (me->query_temp("xmaze7")) me->delete_temp("xmaze7");
+    if (me->query_temp("xmaze8")) me->delete_temp("xmaze8");
+    if (me->query_temp("xmaze9")) me->delete_temp("xmaze9");
+    return 1;
+}
