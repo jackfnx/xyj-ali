0bdf7d879b5b5380b6384a83163ae3148c073d10
NewX <newx@soumo.info>
2011-05-08 10:09:49 +0800

    dest msg

--- cmds/wiz/dest.c
+++ cmds/wiz/dest.c
@@ -9,53 +9,53 @@ inherit F_CLEAN_UP;
 
 int main(object me, string arg)
 {
-   string option, target, msg;
-   object obj,*body,ob;
-   int i;
+    string option, target, msg;
+    object obj,*body,ob;
+    int i;
 
-   if (!arg) return notify_fail("指令格式 : dest <物件之名称或档名>\n" );
+    if (!arg) return notify_fail("指令格式 : dest <物件之名称或档名>\n" );
 
-   if( sscanf(arg, "%s %s", option, target)!=2 ) target = arg;
-if ( option != "-r" && option != "-p" ) target = arg;
+    if (sscanf(arg, "%s %s", option, target)!=2) target = arg;
+    if (option != "-r" && option != "-p") target = arg;
 
-// added by mon. 3/12/97
-        if(arg=="-p") {
-     body=children(USER_OB);
-     for (i=0;i<sizeof(body);i++){
-       ob=body[i];
-       if(clonep(ob)&&ob->query("max_gin")==0)
-         write(getuid(ob)+" "+ob->short()+"\n");
-          }
-          return 1;
+    // added by mon. 3/12/97
+    if (arg == "-p") {
+        body = children(USER_OB);
+        for (i=0; i<sizeof(body); i++) {
+            ob = body[i];
+            if (clonep(ob) && ob->query("max_gin")==0)
+                write(getuid(ob)+" "+ob->short()+"\n");
         }
+        return 1;
+    }
 
-        if( option == "-r" && (string)SECURITY_D->get_status(me)=="(admin)" )
-     seteuid(ROOT_UID);
-   else
-     seteuid(geteuid(me));
+    if (option == "-r" && (string)SECURITY_D->get_status(me)=="(admin)")
+        seteuid(ROOT_UID);
+    else
+        seteuid(geteuid(me));
 
-   obj = find_object(target);
-   if (!obj) obj = present(target, me);
-   if (!obj) obj = present(target, environment(me));
-   if (!obj) obj = find_object( resolve_path(me->query("cwd"), target) );
-   if (!obj) return notify_fail("没有这样物件。\n");
+    obj = find_object(target);
+    if (!obj) obj = present(target, me);
+    if (!obj) obj = present(target, environment(me));
+    if (!obj) obj = find_object( resolve_path(me->query("cwd"), target));
+    if (!obj) return notify_fail("没有这样物件。\n");
 
-   if( environment(me)==environment(obj) )
-   if( !stringp(msg = me->query("env/msg_dest")) )
-     msg = "$N召唤出一个黑洞，将$n吞没了。\n";
-        else 
-     message_vision(msg + "\n", me, obj);
+    if (environment(me)==environment(obj)) {
+        if (!stringp(msg = me->query("env/msg_dest")))
+            msg = "$N召唤出一个黑洞，将$n吞没了。";
+        message_vision(msg + "\n", me, obj);
+    }
+    printf("摧毁物件：%O\n", obj);
+    destruct(obj);
+    if (obj) write("你无法将这个物件摧毁。\n");
+    else write("Ok.\n");
 
-   destruct(obj);
-   if(obj) write("你无法将这个物件摧毁。\n");
-   else write("Ok.\n");
-
-   return 1;
+    return 1;
 }
 
 int help(object me)
 {
-   write(@HELP
+    write(@HELP
 指令格式 : dest [-r] <物件之名称或档名>
            dest [-p] show damaged player objects.
 
