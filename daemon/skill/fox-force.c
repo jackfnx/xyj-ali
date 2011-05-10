44dacccead53dd130da2a596843b0d6464c64b7b
NewX <NewX@soumo.info>
2011-05-10 16:14:53 +0800

    yilou_force

--- daemon/skill/fox-force.c
+++ daemon/skill/fox-force.c
@@ -7,12 +7,6 @@ string name() { return "天狐心法"; }
 
 int valid_enable(string usage) { return usage=="force"; }
 
-int practice_skill(object me)
-{
-    return
-        notify_fail("天狐心法只能向师父学，或是从运用(exert/cast)中增加熟练度。\n");
-}
-
 string cast_spell_file(string spell)
 {
     return CLASS_D("gumu") + "/fox-force/" + spell;
